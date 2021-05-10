87a334a3c5a603dd1a37e899c2a5164791adc828
Hetpatel-2399 <hetpatel23299@gmail.com>
2021-05-10 19:11:49 -0400

    Backend IOT Code

---
+++ IOT Sensor Code/temp/WiFi/WiFi_Client_Server/SW4STM32/B-L475E-IOT01/Debug/Application/WIFI/es_wifi.d
@@ -0,0 +1,7 @@
+Application/WIFI/es_wifi.o: \
+ C:/Users/HET\ PATEL/STM32Cube/Repository/STM32Cube_FW_L4_V1.16.0/Projects/B-L475E-IOT01A/Applications/WiFi/Common/Src/es_wifi.c \
+ ../../../../Common/Inc/es_wifi.h ../../../Inc/es_wifi_conf.h
+
+../../../../Common/Inc/es_wifi.h:
+
+../../../Inc/es_wifi_conf.h:

47dd3eaeb93a53153254b7d3bf555f50a7c74c8d
Hetpatel-2399 <hetpatel23299@gmail.com>
2021-05-10 18:59:36 -0400

    Front end development

---
+++ Front End/acce.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!TEAM: HET,RITWIKK, KON>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/reset.css" media="screen">
+    <link rel="stylesheet" href="css/main.css" media="screen">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:900&display=swap" rel="stylesheet">
+    <title>MACHINE | Our Process </title>
+  </head>
+  <body class="contactBG processPage">
+      <header class="headCon">
+        <h2 class="hidden">Header Container</h2>
+          <nav class="nav">
+            <h3 class="hidden">Navigation Bar Container</h3>
+            <div class="logo">
+                <h4 class="hidden">Logo</h4>
+                <img src="images/f.webp" alt="logo">
+            </div>
+      			<ul class="nav-links contact-links">
+      				<li><a href="index.html" class="navHover conHover ">HOME</a></li>
+      				<li><a href="about.html" class="navHover conHover">ABOUT</a></li>
+      				<li><a href="process.html" class="navHover conHover active">PROCESS</a></li>
+      				<li><a href="contact.html" class="navHover conHover">FEEDBACK</a></li>
+      			</ul>
+           
+    		</nav>
+      </header>
+      <main>
+          <h2 class="hidden">Main Body Container</h2>
+          <section class="aboutTitle">
+            <h3 class="hidden">Who Are We Title</h3>
+            <p>Accelerometer Data</p>
+          </section>
+</main>
+</body>
+</html>
